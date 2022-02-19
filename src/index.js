5a68038d7d22defeb16eda4ae3886d78f0703250
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-19 14:38:05 +0600

    ddd

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import {configureStore} from '@reduxjs/toolkit'
+import userReducer from './features/Users'
+
+const store =configureStore({
+  reducer:{
+    user:userReducer,
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>

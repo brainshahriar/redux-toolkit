576ac9c0c5dc6cc9549ba0a98a3ca09f83878d9b
shahriar <shahriar.mehedi@bjitgroup.com>
2022-02-20 19:59:27 +0600

    app

--- crud_api/src/App.js
+++ crud_api/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useGetPostByIdQuery } from './services/post';
 function App() {
+  // const responseInfo = useGetAllPostQuery()
+   const responseInfo = useGetPostByIdQuery(11)
+  console.log("Response Information : ",responseInfo)
+  console.log("Data : ",responseInfo.data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <h1>RTK -QUERY</h1>
+        {
+          responseInfo.data.map((post,i)=>
+            <div key={i}>
+              <h2>{post.id} {post.title}</h2>
+              <p>{post.body}</p>
+           </div>
+          )
+        } */}
+        <h2>{responseInfo.data.id} {responseInfo.data.title}</h2>
+        <p>{responseInfo.data.body}</p>
     </div>
   );
 }
---
+++ crud_api/src/services/post.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const postApi = createApi({
+    reducerPath: 'postApi',
+    baseQuery : fetchBaseQuery({
+        baseUrl : 'https://jsonplaceholder.typicode.com/'
+    }),
+
+    endpoints:(builder)=>({
+        getAllPost:builder.query({
+            query:()=>({
+                url : 'posts',
+                method:'GET'
+            })
+        }),
+        getPostById:builder.query({
+            query:(id)=>({
+                url : `posts/${id}`,
+                method:'GET'
+            })
+        })
+    })
+})
+
+
+
+export const { useGetAllPostQuer,useGetPostByIdQuery } = postApi
